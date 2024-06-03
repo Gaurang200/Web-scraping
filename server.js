42a1cb127930f608ed9c5dc07fa28d23539ccd96
gaurang <tadhanigaurang@gmail.com>
2024-06-03 22:41:59 +0530

    https://github.com/Gaurang200/Web-scraping.git

--- server.js
+++ server.js
@@ -1,43 +1,67 @@
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-const path = require("path");
-
+const path1 = require("path");
 puppeteer.use(StealthPlugin());
 
 async function createDriver(curpValue) {
   let browser;
   try {
-    const extensionPath = path.join(__dirname, "config.js");
-    browser = await puppeteer.launch({
+    const path = path1.join(__dirname, "config.js");
+    const browser = await puppeteer.launch({
       headless: true,
-      args: [`--disable-extensions-except=${extensionPath}`],
-    });
-    const page = await browser.newPage();
+      args: [`--disable-extensions-except=${path}`, ],
+    });               
+    var page = await browser.newPage();
     await page.goto("https://www.gob.mx/curp/");
     await page.waitForSelector("#curpinput");
     await page.waitForSelector("#searchButton");
     await page.type("#curpinput", curpValue);
     await page.click("#searchButton");
+    // await page.waitForNavigation();
+
 
     const updatedValue = await page.$eval("#curpinput", (input) => input.value);
     console.log("Updated value:", updatedValue);
-
     await page.waitForSelector(".col-md-7");
 
     const curpInfo = await page.$eval(
       ".col-md-7 .panel .panel-body table:first-child",
-      (table) => extractTableData(table)
+      (table) => {
+        const info = {};
+        const rows = table.querySelectorAll("tr");
+        rows.forEach((row) => {
+          const label = row.querySelector("td:first-child").textContent.trim();
+          const value = row.querySelector("td:last-child").textContent.trim();
+          info[label] = value;
+        });
+        return info;
+      }
     );
-
     const curpInfo1 = await page.$eval(
       ".col-md-5 .panel .panel-body table:last-child",
-      (table) => extractTableData(table)
+      (table) => {
+        const info = {};
+        const rows = table.querySelectorAll("tr");
+        rows.forEach((row) => {
+          const label = row.querySelector("td:first-child").textContent.trim();
+          const value = row.querySelector("td:last-child").textContent.trim();
+          info[label] = value;
+        });
+        return info;
+      }
     );
-
     const data = { ...curpInfo, ...curpInfo1 };
     console.log(data, "curpInfo");
   } catch (error) {
     console.error("An error occurred:", error);
+    await page.waitForSelector("#curpinput");
+    await page.waitForSelector("#searchButton");
+
+    await page.type("#curpinput", curpValue);
+    await page.click("#searchButton");
+
+   
+    // await browser.close();
   } finally {
     if (browser) {
       await browser.close();
@@ -45,15 +69,5 @@ async function createDriver(curpValue) {
   }
 }
 
-function extractTableData(table) {
-  const info = {};
-  const rows = table.querySelectorAll("tr");
-  rows.forEach((row) => {
-    const label = row.querySelector("td:first-child").textContent.trim();
-    const value = row.querySelector("td:last-child").textContent.trim();
-    info[label] = value;
-  });
-  return info;
-}
-
 createDriver("SIBR730704HJCQRB05");
+
